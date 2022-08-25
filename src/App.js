7b7ceb34fc0be0667472c186813ae11a4144958a
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2022-08-25 16:25:47 +0600

    react project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+
+  return (
+    <div className="">
+      <p className="text-red-600">hi</p>
+
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
